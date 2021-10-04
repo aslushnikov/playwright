22abb8aed197f73ba4feed3aabb77def987fdd77
Andrey Lushnikov <lushnikov@chromium.org>
2021-10-04 16:17:49 -0700

    browser(ff-beta): roll Firefox Beta to v94.0b1

--- browser_patches/firefox-beta/UPSTREAM_CONFIG.sh
+++ browser_patches/firefox-beta/UPSTREAM_CONFIG.sh
@@ -1,3 +1,3 @@
 REMOTE_URL="https://github.com/mozilla/gecko-dev"
 BASE_BRANCH="beta"
-BASE_REVISION="3793d70f36e182746eeb0621c07f5c35bb5977f5"
+BASE_REVISION="638fd89d849b0e785d05595422c82cd62aadffa7"
