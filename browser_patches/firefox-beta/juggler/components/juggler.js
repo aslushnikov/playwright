224f8a5c10eb80f4fcd6029294e622d0dc1f9a77
Andrey Lushnikov <lushnikov@chromium.org>
2022-08-02 17:31:10 +0300

    browser(ff-beta): roll Firefox Beta to 104.0b5
    This roll moves Juggler component registration to a new system that
    is used in XPCOM: this is what both RemoteAgent and Marionette use.
    
    Drive-by: we now have to explicitly pass default values to dispatched
    mouse events to avoid a `MOZ_ASSERT` trigger inside a drag service
    native code.
    
    Manual conflict resolutions: https://github.com/aslushnikov/juggler/commit/ab3ed0cb2c2ecebf0fa11ac93ac42b5af10851a7

--- browser_patches/firefox-beta/juggler/components/juggler.js
+++ browser_patches/firefox-beta/juggler/components/juggler.js
@@ -2,6 +2,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+var EXPORTED_SYMBOLS = ["Juggler", "JugglerFactory"];
+
 const {XPCOMUtils} = ChromeUtils.import("resource://gre/modules/XPCOMUtils.jsm");
 const {ComponentUtils} = ChromeUtils.import("resource://gre/modules/ComponentUtils.jsm");
 const {Services} = ChromeUtils.import("resource://gre/modules/Services.jsm");
@@ -17,115 +19,111 @@ const Ci = Components.interfaces;
 
 const FRAME_SCRIPT = "chrome://juggler/content/content/main.js";
 
-// Command Line Handler
-function CommandLineHandler() {
-};
+class Juggler {
+  get classDescription() { return "Sample command-line handler"; }
+  get classID() { return Components.ID('{f7a74a33-e2ab-422d-b022-4fb213dd2639}'); }
+  get contractID() { return "@mozilla.org/remote/juggler;1" }
+  get QueryInterface() {
+    return ChromeUtils.generateQI([ Ci.nsICommandLineHandler, Ci.nsIObserver ]);
+  }
+  get helpInfo() {
+    return "  --juggler            Enable Juggler automation\n";
+  }
 
-CommandLineHandler.prototype = {
-  classDescription: "Sample command-line handler",
-  classID: Components.ID('{f7a74a33-e2ab-422d-b022-4fb213dd2639}'),
-  contractID: "@mozilla.org/remote/juggler;1",
-  _xpcom_categories: [{
-    category: "command-line-handler",
-    entry: "m-juggler"
-  }],
+  handle(cmdLine) {
+    // flag has to be consumed in nsICommandLineHandler:handle
+    // to avoid issues on macos. See Marionette.jsm::handle() for more details.
+    // TODO: remove after Bug 1724251 is fixed.
+    cmdLine.handleFlag("juggler-pipe", false);
+  }
 
-  /* nsICommandLineHandler */
-  handle: async function(cmdLine) {
-    const jugglerFlag = cmdLine.handleFlagWithParam("juggler", false);
-    const jugglerPipeFlag = cmdLine.handleFlag("juggler-pipe", false);
-    if (!jugglerPipeFlag && (!jugglerFlag || isNaN(jugglerFlag)))
-      return;
-    const silent = cmdLine.preventDefault;
-    if (silent)
-      Services.startup.enterLastWindowClosingSurvivalArea();
+  async observe(subject, topic) {
+    switch (topic) {
+      case "profile-after-change":
+        Services.obs.addObserver(this, "command-line-startup");
+        break;
+      case "command-line-startup":
+        Services.obs.removeObserver(this, topic);
+        const cmdLine = subject;
+        const jugglerPipeFlag = cmdLine.handleFlag('juggler-pipe', false);
+        if (!jugglerPipeFlag)
+          return;
+        Services.startup.enterLastWindowClosingSurvivalArea();
+        Services.obs.addObserver(this, "final-ui-startup");
+        Services.obs.addObserver(this, "browser-idle-startup-tasks-finished");
+        Services.obs.addObserver(this, "mail-idle-startup-tasks-finished");
+        Services.obs.addObserver(this, "quit-application");
+        break;
+      // Used to wait until the initial application window has been opened.
+      case "final-ui-startup":
+        Services.obs.removeObserver(this, topic);
 
-    const targetRegistry = new TargetRegistry();
-    new NetworkObserver(targetRegistry);
+        const targetRegistry = new TargetRegistry();
+        new NetworkObserver(targetRegistry);
 
-    const loadFrameScript = () => {
-      Services.mm.loadFrameScript(FRAME_SCRIPT, true /* aAllowDelayedLoad */);
-      if (Cc["@mozilla.org/gfx/info;1"].getService(Ci.nsIGfxInfo).isHeadless) {
-        const styleSheetService = Cc["@mozilla.org/content/style-sheet-service;1"].getService(Components.interfaces.nsIStyleSheetService);
-        const ioService = Cc["@mozilla.org/network/io-service;1"].getService(Components.interfaces.nsIIOService);
-        const uri = ioService.newURI('chrome://juggler/content/content/hidden-scrollbars.css', null, null);
-        styleSheetService.loadAndRegisterSheet(uri, styleSheetService.AGENT_SHEET);
-      }
-    };
-
-    // Force create hidden window here, otherwise its creation later closes the web socket!
-    Services.appShell.hiddenDOMWindow;
-
-    if (jugglerFlag) {
-      const port = parseInt(jugglerFlag, 10);
-      const { require } = ChromeUtils.import("resource://devtools/shared/Loader.jsm");
-      const WebSocketServer = require('devtools/server/socket/websocket-server');
-      this._server = Cc["@mozilla.org/network/server-socket;1"].createInstance(Ci.nsIServerSocket);
-      this._server.initSpecialConnection(port, Ci.nsIServerSocket.KeepWhenOffline | Ci.nsIServerSocket.LoopbackOnly, 4);
-      const token = helper.generateId();
-      this._server.asyncListen({
-        onSocketAccepted: async(socket, transport) => {
-          const input = transport.openInputStream(0, 0, 0);
-          const output = transport.openOutputStream(0, 0, 0);
-          const webSocket = await WebSocketServer.accept(transport, input, output, "/" + token);
-          const dispatcher = new Dispatcher(webSocket);
-          const browserHandler = new BrowserHandler(dispatcher.rootSession(), dispatcher, targetRegistry, () => {
-            if (silent)
-              Services.startup.exitLastWindowClosingSurvivalArea();
-          });
-          dispatcher.rootSession().setHandler(browserHandler);
-        }
-      });
-      loadFrameScript();
-      dump(`Juggler listening on ws://127.0.0.1:${this._server.port}/${token}\n`);
-    } else if (jugglerPipeFlag) {
-      let browserHandler;
-      let pipeStopped = false;
-      const pipe = Cc['@mozilla.org/juggler/remotedebuggingpipe;1'].getService(Ci.nsIRemoteDebuggingPipe);
-      const connection = {
-        QueryInterface: ChromeUtils.generateQI([Ci.nsIRemoteDebuggingPipeClient]),
-        receiveMessage(message) {
-          if (this.onmessage)
-            this.onmessage({ data: message });
-        },
-        disconnected() {
-          if (browserHandler)
-            browserHandler['Browser.close']();
-        },
-        send(message) {
-          if (pipeStopped) {
-            // We are missing the response to Browser.close,
-            // but everything works fine. Once we actually need it,
-            // we have to stop the pipe after the response is sent.
-            return;
+        const loadFrameScript = () => {
+          Services.mm.loadFrameScript(FRAME_SCRIPT, true /* aAllowDelayedLoad */);
+          if (Cc["@mozilla.org/gfx/info;1"].getService(Ci.nsIGfxInfo).isHeadless) {
+            const styleSheetService = Cc["@mozilla.org/content/style-sheet-service;1"].getService(Components.interfaces.nsIStyleSheetService);
+            const ioService = Cc["@mozilla.org/network/io-service;1"].getService(Components.interfaces.nsIIOService);
+            const uri = ioService.newURI('chrome://juggler/content/content/hidden-scrollbars.css', null, null);
+            styleSheetService.loadAndRegisterSheet(uri, styleSheetService.AGENT_SHEET);
           }
-          pipe.sendMessage(message);
-        },
-      };
-      pipe.init(connection);
-      const dispatcher = new Dispatcher(connection);
-      browserHandler = new BrowserHandler(dispatcher.rootSession(), dispatcher, targetRegistry, () => {
-        if (silent)
+        };
+
+        // Force create hidden window here, otherwise its creation later closes the web socket!
+        Services.appShell.hiddenDOMWindow;
+
+        let pipeStopped = false;
+        const pipe = Cc['@mozilla.org/juggler/remotedebuggingpipe;1'].getService(Ci.nsIRemoteDebuggingPipe);
+        const connection = {
+          QueryInterface: ChromeUtils.generateQI([Ci.nsIRemoteDebuggingPipeClient]),
+          receiveMessage(message) {
+            if (this.onmessage)
+              this.onmessage({ data: message });
+          },
+          disconnected() {
+            if (this._browserHandler)
+              this._browserHandler['Browser.close']();
+          },
+          send(message) {
+            if (pipeStopped) {
+              // We are missing the response to Browser.close,
+              // but everything works fine. Once we actually need it,
+              // we have to stop the pipe after the response is sent.
+              return;
+            }
+            pipe.sendMessage(message);
+          },
+        };
+        pipe.init(connection);
+        const dispatcher = new Dispatcher(connection);
+        this._browserHandler = new BrowserHandler(dispatcher.rootSession(), dispatcher, targetRegistry, () => {
           Services.startup.exitLastWindowClosingSurvivalArea();
-        connection.onclose();
-        pipe.stop();
-        pipeStopped = true;
-      });
-      dispatcher.rootSession().setHandler(browserHandler);
-      loadFrameScript();
-      dump(`\nJuggler listening to the pipe\n`);
+          connection.onclose();
+          pipe.stop();
+          pipeStopped = true;
+        });
+        dispatcher.rootSession().setHandler(this._browserHandler);
+        loadFrameScript();
+        dump(`\nJuggler listening to the pipe\n`);
+        break;
+      case "browser-idle-startup-tasks-finished":
+      case "mail-idle-startup-tasks-finished":
+        Services.obs.removeObserver(this, "browser-idle-startup-tasks-finished");
+        Services.obs.removeObserver(this, "mail-idle-startup-tasks-finished");
+        break;
+      case "quit-application":
+        break;
     }
-  },
+  }
 
-  QueryInterface: ChromeUtils.generateQI([ Ci.nsICommandLineHandler ]),
+}
 
-  // CHANGEME: change the help info as appropriate, but
-  // follow the guidelines in nsICommandLineHandler.idl
-  // specifically, flag descriptions should start at
-  // character 24, and lines should be wrapped at
-  // 72 characters with embedded newlines,
-  // and finally, the string should end with a newline
-  helpInfo : "  --juggler            Enable Juggler automation\n"
+const jugglerInstance = new Juggler();
+
+// This is used by the XPCOM codepath which expects a constructor
+var JugglerFactory = function() {
+  return jugglerInstance;
 };
 
-var NSGetFactory = ComponentUtils.generateNSGetFactory([CommandLineHandler]);
